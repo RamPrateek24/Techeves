956de18ff925f9efd2d5d56b15f2433c6bffc8cf
Ram Prateek <ramprateekmantripragada@gmail.com>
2025-10-29 16:20:01 +0530

    v16 adds

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
-
+import events, {EventItem} from "@/lib/constants";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async() => {
   'use cache';
   cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
   return (
     <section>
@@ -19,7 +19,7 @@ const page = async() => {
       <div className="mt-20 space-y-7"> 
         <h3>Featured Events</h3>
         <ul className = "events">
-          {events && events.length > 0 && events.map((event:IEvent)=>(
+          {events && events.length > 0 && events.map((event:EventItem)=>(
             <li key = {event.title} className="list-none"><EventCard {...event}/></li>
           ))}
         </ul>
